c10e57c8a3148609cb6670abfb848c6d4b976287
Lucidreline <casta.ma502@gmail.com>
2022-04-08 22:29:19 -0700

    Added a FindBySKU, Create, and Delete route.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
+const cors = require('cors')
 const express = require('express')
 
 const app = express()
 
 require('./config/connectDB')()
 
+app.use(cors())
+app.use(express.json())
 app.use('/clothing', require('./routes/clothing'))
 
 const PORT = 8080
--- src/routes/clothing.js
+++ src/routes/clothing.js
@@ -7,13 +7,40 @@ const Clothing = require('../models/Clothing')
 router.get('/', (req, res) => {
   Clothing.find()
     .then((allClothing) => res.json({ results: allClothing }))
-    .catch((error) => res.json({ error }))
+    .catch((error) => res.json({ error: error.message }))
 })
 
 // get one clothing by SKU
+router.get('/:sku', (req, res) => {
+  const { sku } = req.params
+
+  Clothing.findOne({ 'attributes.sku': sku })
+    .then((searchResult) => {
+      if (searchResult) res.json({ result: searchResult })
+      else
+        res.json({
+          error: `No results found when searching for SKU ${sku}, ${searchResult}`,
+        })
+    })
+    .catch((error) => res.json({ error: error.message }))
+})
 
 // add clothing
+router.post('/', (req, res) => {
+  const newClothing = req.body
+
+  Clothing.create(newClothing)
+    .then((createdClothing) => res.json({ createdClothing }))
+    .catch((error) => res.json({ error: error.message }))
+})
 
 // delete clothing by id
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+
+  Clothing.findByIdAndDelete(id)
+    .then((result) => res.json({ result }))
+    .catch((error) => res.json({ error: error.message }))
+})
 
 module.exports = router
