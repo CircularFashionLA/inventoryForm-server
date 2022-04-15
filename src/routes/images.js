32bcfd002de2ce23d9f409929e4e54553280cfb8
Lucidreline <casta.ma502@gmail.com>
2022-04-15 16:06:31 -0700

    Refactored how cloudinary images are uploaded.

--- src/routes/images.js
+++ src/routes/images.js
@@ -1,24 +1,17 @@
 const express = require('express')
-require('dotenv').config()
-const cloudinary = require('cloudinary').v2
-
-cloudinary.config({
-  cloud_name: process.env.cloudinary_name,
-  api_key: process.env.cloudinary_key,
-  api_secret: process.env.cloudinary_secret,
-  secure: true,
-})
+const cloudinary = require('../utils/cloudinary')
 
 const router = express.Router()
 
 router.post('/geturl', async (req, res) => {
   const fileStr = req.body.data
 
-  const uploadedResponce = await cloudinary.uploader.upload(fileStr, {
-    upload_preset: 'cfla-inventory',
-  })
-  console.log(uploadedResponce)
-  res.json({ msg: 'hi there front end' })
+  cloudinary.uploader
+    .upload(fileStr, {
+      upload_preset: 'cfla-inventory',
+    })
+    .then((uploadRes) => res.json({ url: uploadRes.url }))
+    .catch((error) => res.json({ error: error.message }))
 })
 
 module.exports = router
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+  cloud_name: process.env.cloudinary_name,
+  api_key: process.env.cloudinary_key,
+  api_secret: process.env.cloudinary_secret,
+  secure: true,
+})
+
+module.exports = cloudinary
