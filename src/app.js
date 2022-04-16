0a8a9d796e1baddced8f324753b518120137d9be
Lucidreline <casta.ma502@gmail.com>
2022-04-15 17:42:18 -0700

    Trying to fix CORS issue.

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ const app = express()
 require('./config/connectDB')()
 
 app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
 app.use(express.json({ limit: '50mb' }))
 app.use('/clothing', require('./routes/clothing'))
 app.use('/images', require('./routes/images'))
