9b325bdafc5454b55fa7d7b2bc8dd34391fe18ad
Lucidreline <casta.ma502@gmail.com>
2022-04-17 15:51:36 -0700

    Adding logs to see why images won't upload.

--- src/routes/images.js
+++ src/routes/images.js
@@ -4,14 +4,18 @@ const cloudinary = require('../utils/cloudinary')
 const router = express.Router()
 
 router.post('/geturl', (req, res) => {
-  const fileStr = req.body.data
+  try {
+    const fileStr = req.body.data
 
-  cloudinary.uploader
-    .upload(fileStr, {
-      upload_preset: 'cfla-inventory',
-    })
-    .then((uploadRes) => res.json({ url: uploadRes.url }))
-    .catch((error) => res.json({ error: error.message }))
+    cloudinary.uploader
+      .upload(fileStr, {
+        upload_preset: 'cfla-inventory',
+      })
+      .then((uploadRes) => res.json({ url: uploadRes.url }))
+      .catch((error) => res.json({ error: error.message }))
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 module.exports = router
