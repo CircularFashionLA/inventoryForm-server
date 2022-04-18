0ac837a0f13a336e1bf3aae7e5efb5ca67584c27
Lucidreline <casta.ma502@gmail.com>
2022-04-17 18:59:53 -0700

    Removed logging since I fixed cors issue.

--- src/routes/images.js
+++ src/routes/images.js
@@ -5,20 +5,15 @@ const router = express.Router()
 
 router.post('/geturl', (req, res) => {
   try {
-    console.log('made it to geturl')
     const fileStr = req.body.data
-    console.log(`req.body: ${fileStr}`)
-
     cloudinary.uploader
       .upload(fileStr, {
         upload_preset: 'cfla-inventory',
       })
       .then((uploadRes) => res.json({ url: uploadRes.url }))
   } catch (error) {
-    console.log(error)
-    res.send('oh my gosh... pls work')
+    res.json({ error })
   }
-  console.log('after the try catch')
 })
 
 module.exports = router
